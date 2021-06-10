2bc034b8a97c75fd84a489bf4db405eb17413bce
LinkGian <59111263+LinkGian@users.noreply.github.com>
2021-06-10 14:42:44 -0600

    Update name.py
    Add first_name last_name along with print functions

--- name.py
+++ name.py
@@ -1,5 +1,13 @@
-name = "Ada Lovelace"
-print(name)
+#name = "Ada Lovelace"
+#print(name)
 
-print(name.lower)
-print(name.upper)
+#print(name.lower)
+#print(name.upper)
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("Languages:\nPython\nC\nJavaScript")
