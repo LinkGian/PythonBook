d685963ee312f4025c6413da519e26b828f6b070
LinkGian <59111263+LinkGian@users.noreply.github.com>
2021-06-10 16:48:33 -0600

    Create exchanges.py
    create exchanges list and manipulate it to create messages using exchanges listed

---
+++ exchanges.py
@@ -0,0 +1,5 @@
+exchanges = ['Coinbase', 'Binance', 'FTX', 'Kraken', 'Bittrex']
+message = f"My favorite exchange is {exchanges[2]}..."
+message2 = f"and {exchanges[1]} is trash."
+print(message)
+print(message2)
