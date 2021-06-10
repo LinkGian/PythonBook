f34a7622269e68ce14133ea1db63532c18650eb8
LinkGian <59111263+LinkGian@users.noreply.github.com>
2021-06-10 15:41:14 -0600

    Update Famous_Quote.py
    - [] add SN and change name variable

--- Famous_Quote.py
+++ Famous_Quote.py
@@ -1,4 +1,4 @@
 Quote = "We make open-source software and we think we make it so well it's likely to become a standard."
-Name = "Sergey Nazarov"
-message = f"{Name} once said, {Quote}"
+famous_person = "Sergey Nazarov"
+message = f"{famous_person} once said, {Quote}"
 print(message)
