fbfe197c8a406c77b539914f9c82a2ecb0d2f00a
LinkGian <59111263+LinkGian@users.noreply.github.com>
2021-06-10 18:29:56 -0600

    Update exchanges.py

--- exchanges.py
+++ exchanges.py
@@ -3,3 +3,5 @@ message = f"My favorite exchange is {exchanges[2]}..."
 message2 = f"and {exchanges[1]} is trash."
 print(message)
 print(message2)
+exchanges.append('Bitmex') #append to add to end of list without modifying other elements of list
+print(exchanges)
