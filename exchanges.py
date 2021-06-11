c695139d9ca3dfb302f1745659eb9a9df22a355a
LinkGian <59111263+LinkGian@users.noreply.github.com>
2021-06-10 22:41:43 -0600

    Create exchanges
    List exchanges and coins

--- exchanges.py
+++ exchanges.py
@@ -5,3 +5,10 @@ print(message)
 print(message2)
 exchanges.append('Bitmex') #append to add to end of list without modifying other elements of list
 print(exchanges)
+
+coins = ['LINK', 'WOO', 'FTT', 'AAVE', 'BNT', 'RSR', 'DOT']
+coins.sort()
+print(coins)
+
+message3 = f"If you want to make it, buy {coins[4]}"
+print(message3)
